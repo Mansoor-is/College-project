4944fbc6bab1ebe179618aa204373e7d29a8a0d1
Mansoor-is <mansoorkhanyt76@gmail.com>
2024-08-25 15:45:46 +0500

    code

--- login pages/java.js
+++ login pages/java.js
@@ -4,11 +4,8 @@ let icon = document.querySelector(".icon");
 icon.addEventListener("click", () => {
     if (pass.getAttribute("type") === "password") {
         pass.setAttribute("type", "text");
-        icon.classList.add("fa-eye-slash")
-        icon.classList.remove("fa-eye");
     } else {
         pass.setAttribute("type", "password");
-        icon.classList.add("fa-eye");
-        icon.classList.remove("fa-eye-slash")
     }
 });
+// log in
